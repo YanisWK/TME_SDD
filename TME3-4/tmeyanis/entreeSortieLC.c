4d948c3462cc3d776895df063bf925f42ce4740e
YanisWK <yanis.saadi03@gmail.com>
2024-02-08 12:15:44 +0100

    oui

--- TME3-4/tmeyanis/entreeSortieLC.c
+++ TME3-4/tmeyanis/entreeSortieLC.c
@@ -4,8 +4,26 @@
 #include "biblioLC.h"
 
 Biblio* charger_n_entrees(char* nomfic, int n){
-    File *f=fopen(nomfic,)
+    FILE *f=fopen(nomfic,'r');
+    int num;
+    char* titre;
+    char* auteur;
+    Biblio *newBiblio=creer_biblio();
+
+    if(f==NULL){
+        printf("Erreur");
+        return NULL;
+    }
+    char buffer[256];
+    while(n>0){
+        fgets(buffer,256,f);
+        sscanf(buffer,"%d %s %s",&num,titre,auteur);
+
+    }
+
+
 }
+
 
 
 
