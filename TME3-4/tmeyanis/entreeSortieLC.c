f555b7e8d62fb5e70c4823851927f38c08b01d78
YanisWK <yanis.saadi03@gmail.com>
2024-02-08 11:28:29 +0100

    change

---
+++ TME3-4/tmeyanis/entreeSortieLC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "biblioLC.h"
+
+Biblio* charger n entrees(char* nomfic, int n){
+    
+}
+
+
+
+
+
+void enregistrer biblio(Biblio *b, char* nomfic);
--- tmeines/TME3-4/entreeSortieLC.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "biblioLC.h"
-
-Biblio* charger n entrees(char* nomfic, int n);
-void enregistrer biblio(Biblio *b, char* nomfic);
