c31c1c0a0fde40b1df99d1a0a9bbb0257ed44141
YanisWK <yanis.saadi03@gmail.com>
2024-02-16 17:31:42 +0100

    Creation des fichiers

---
+++ TME3-4/tmerendu/Exo2/biblioLC.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct livre {
+
+    int num ;
+    char * titre ;
+    char * auteur ;
+    struct livre * suiv ;
+} Livre ;
+
+typedef struct{ /* Tete fictive */
+    Livre * L ; /* Premier element */
+} Biblio ;
+
+void liberer_livre(Livre *l);
+Biblio* creer_biblio();
+void liberer_biblio(Biblio* b);
+void inserer_en_tete(Biblio* b,int num,char* titre,char* auteur);
+
+void afficheLivre(Livre *l);
+void afficherBiblio(Biblio *b);
+
+Livre* rechercheLivreNum(Biblio *b,int num);
+Livre* rechercheLivreTitre(Biblio *b,char* titre);
+Biblio* BiblioAuteur(Biblio*b,char* auteur);
+void supprimerLivre(Biblio* b, int num, char* titre, char* auteur);
+Biblio* fusion(Biblio* b1,Biblio* b2);
+Biblio* RecherchePlusieurs(Biblio *b);
