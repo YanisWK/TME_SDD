24ce31b3b9e34decec74b5e14847bae21ebbc3a6
vntgogh <ines.ediane29@gmail.com>
2024-02-19 21:18:54 +0100

    erreur parametre du .h

--- TME3-4/tmerendu/biblioH.h
+++ TME3-4/tmerendu/biblioH.h
@@ -29,6 +29,6 @@ void afficherBiblio_H(BiblioH* b);
 LivreH* rechercheLivreNum_H(BiblioH *b,int num);
 LivreH* rechercheLivreTitre_H(BiblioH* b, char* titre);
 BiblioH* LivresAuteurs_H(BiblioH* b, char* auteur);
-void supprimerLivre_H(BiblioH* b, int clef);
+void supprimerLivre_H(BiblioH *b, int num, char *titre, char *auteur);
 BiblioH* fusion_H(BiblioH* b1,BiblioH* b2);
 BiblioH* RecherchePlusieurs_H(BiblioH *b);
