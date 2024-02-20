455ed5fdd5e55deb37feadfad0c059dabe171711
vntgogh <ines.ediane29@gmail.com>
2024-02-20 21:05:51 +0100

    SHEESH

--- TME3-4/tmerendu/main.c
+++ TME3-4/tmerendu/main.c
@@ -38,30 +38,6 @@ int main(int argc, char** argv){
     
     // enregistrer_biblio(b, "saved.txt");
 
-    // Biblio *b1= creer_biblio_LC();
-    // Biblio *b2= creer_biblio_LC();
-
-    // inserer_en_tete_LC(b1,1,"Spy x Family 1","Tatsuya Endo");
-    // inserer_en_tete_LC(b1,2,"Spy x Family 2","Tatsuya Endo");    
-    // inserer_en_tete_LC(b2,3,"Spy x Family 3","Tatsuya Endo");
-    // inserer_en_tete_LC(b2,4,"Spy x Family 4","Tatsuya Endo");
-
-    // Biblio *b3 = fusion_LC(b1,b2);
-    // supprimerLivre_LC(b3,2,"Spy x Family 2","Tatsuya Endo");
-    // afficherBiblio_LC(b3);
-    
-    // inserer_en_tete_LC(b1,3,"Spy x Family 3","Tatsuya Endo");
-    // inserer_en_tete_LC(b1,4,"Spy x Family 2","Tatsuya Endo");
-
-    Biblio *b4 = RecherchePlusieurs_LC(b4);
-    //afficherBiblio_LC(b4);
-    
-    // liberer_biblio_LC(b4);
-    //liberer_biblio_LC(b3);
-    // liberer_biblio_LC(b2);
-    // liberer_biblio_LC(b1);
-
-
     char *entree;
     int rep;
     int choix;
