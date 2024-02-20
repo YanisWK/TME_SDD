919b2a644bb6845eb5c2189ad1765cea6abdb078
YanisWK <yanis.saadi03@gmail.com>
2024-02-20 02:28:17 +0100

    compare

--- TME3-4/tmerendu/compare.c
+++ TME3-4/tmerendu/compare.c
@@ -21,6 +21,12 @@ int main(){
     LivreH* LH4=creer_livre_H(4,"is","ama");
     LivreH* LH5=creer_livre_H(5,"goated","zing");
 
+    Livre *LC1=creer_livre_LC(1,"spy","endo");
+    Livre *LC2=creer_livre_LC(2,"x","tatsu");
+    Livre *LC3=creer_livre_LC(3,"family","ya");
+    Livre *LC4=creer_livre_LC(4,"is","ama");
+    Livre *LC5=creer_livre_LC(5,"goated","zing");
+
 
     
 
