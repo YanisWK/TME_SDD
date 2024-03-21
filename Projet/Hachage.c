443fa5f8a4f05b1469a59505dded6a77a129860b
vntgogh <ines.ediane29@gmail.com>
2024-03-21 11:24:37 +0100

    fontcion cle de hachage

---
+++ Projet/Hachage.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(int x, int y){
+    return y + (x + y)(x + y + 1)/2;
+}
--- Projet/Hachage.h
+++ Projet/Hachage.h
@@ -1,5 +1,10 @@
-typedef struct{
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TH{
   int nbElement; //pas necessaire ici
   int tailleMax;
   CellNoeud** T;
 } TableHachage ;
+
+int f(int x, int y);
