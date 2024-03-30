31a719ce6dd3fd60b71596c34cd2100676d096b8
vntgogh <ines.ediane29@gmail.com>
2024-03-30 20:49:15 +0100

    fichier .h

--- Projet/ArbreQuat.h
+++ Projet/ArbreQuat.h
@@ -15,4 +15,11 @@ typedef struct arbreQuat{
     struct arbreQuat *ne;   /* Sous-arbre nord-est, pour x >= xc et y >= yc */
 } ArbreQuat;
 
+void chaineCoordMinMax(Chaines* C,double* xmin,double* ymin,double *xmax,double *ymax);
+ArbreQuat* creerArbreQuat(double xc,double yc,double coteX,double coteY);
+void insererNoeudArbre(Noeud *n,ArbreQuat ** a,ArbreQuat **parent);
+Noeud* rechercheCreeNoeudArbre(Reseau* R, ArbreQuat** a, ArbreQuat*parent, double x, double y);
+Reseau* reconstitueReseauArbre(Chaines* C);
+void libererArbre(ArbreQuat *a);
+
 #endif
