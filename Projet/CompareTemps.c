4ca245c5fdea6b143aee91b436d72afaa5c7f80c
YanisWK <yanis.saadi03@gmail.com>
2024-04-30 09:06:10 +0200

    Ajout des Graphique et correction du probleme pour Compare Temps

--- Projet/CompareTemps.c
+++ Projet/CompareTemps.c
@@ -19,11 +19,9 @@ int main(){
     
     fprintf(fic4, "%s\t %s\t\t %s\t\t %s\t\t %s\t\t %s\t\n","Nb points","Liste","Hachage 1","Hachage 2","Hachage 3","Arbre");
     for (int i=500; i<=5000; i+=500){
+        srand(time(NULL));
         c = generationAleatoire(i,100,5000,5000);
-        while (c->chaines){
-            nbpoints+=nbPoints(c->chaines->points);
-            c->chaines=c->chaines->suiv;
-        }
+        
 
         // LISTE CHAINEE
         deb = clock();
@@ -65,9 +63,12 @@ int main(){
         fin = clock();
         printf("Fin (Table de hachage) M=%d\n",M);
         tempsHachage3 = ((double)(fin-deb))/CLOCKS_PER_SEC;
-
+        while (c->chaines){
+            nbpoints+=nbPoints(c->chaines->points);
+            c->chaines=c->chaines->suiv;
+        }
         fprintf(fic4, "%d\t\t %.7lf\t %.7lf\t\t %.7lf\t\t %.7lf\t\t %.7lf\n",nbpoints,tempsListe,tempsHachage1,tempsHachage2,tempsHachage3,tempsArbre);
-             
+        printf("\nFin de la Reconstitution pour %d points\n\n",nbpoints);
         nbpoints=0;   
     }
 
